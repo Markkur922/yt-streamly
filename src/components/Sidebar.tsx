e11d373b120bcc99d2c49885ef47195544a2a20f
Markkur922 <markkur32@gmail.com>
2026-09-19 20:51:43 +0500

    change title

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -205,7 +205,7 @@ function SidebarContent({ onNavigate }: { onNavigate?: () => void }) {
       ))}
 
       <p className="mt-4 px-3 text-xs leading-relaxed text-2">
-        Streamly · 
+        Streamly ·
         <br />
         видеоплатформа
       </p>
